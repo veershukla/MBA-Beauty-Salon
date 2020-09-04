6a96395c53eedeebd1dbd53103a9566ef9af1046
Vikas Shukla <shuk.vk@gmail.com>
2020-09-04 16:34:12 -0700

    finishes contact information

--- src/App.js
+++ src/App.js
@@ -30,6 +30,14 @@ function App() {
         <div className="contactInfo">
           <h3>1855 S. La Barbera Ave Mountain House, CA 95391</h3>
           <h3>bindooveerendra@yahoo.com</h3>
+          <div className="openingHours">
+            <h3>OPENING HOURS</h3>
+            <h3>
+              {
+                'Mon - Fri: 7am - 10pm  ​​Saturday: 8am - 10pm  Sunday: 8am - 11pm'
+              }
+            </h3>
+          </div>
         </div>
       </div>
     </div>
