f547f564e60bf8f2d356ca3131f5a76f00ba859c
Vikas Shukla <shuk.vk@gmail.com>
2020-09-04 16:16:53 -0700

    adds title and contact info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test
-        </a>
+      <header className="landingPageHeader">
+        <h1>MBA Beauty Salon</h1>
       </header>
+      <div className="services">
+        {/* <header className="servicesHeader">
+          <h1>Services</h1>
+        </header>
+        <div className="servicesList">
+          <h3>Threading</h3>
+          <h3>Hair and Color</h3>
+          <h3>Waxing</h3>
+          <h3>Facial</h3>
+          <h3>Heena</h3>
+          <h3>Eyelashes</h3>
+          <h3>Microblading</h3>
+          <h3>Nails</h3>
+        </div> */}
+      </div>
+
+      <div className="contact">
+        <header>
+          <h1>Visit Us</h1>
+        </header>
+        <div className="contactInfo">
+          <h3>1855 S. La Barbera Ave Mountain House, CA 95391</h3>
+          <h3>bindooveerendra@yahoo.com</h3>
+        </div>
+      </div>
     </div>
   );
 }
