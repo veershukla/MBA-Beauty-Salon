0f2679e760c7df4bd4663d3ce624e5448b7b8645
Vikas Shukla <shuk.vk@gmail.com>
2020-09-04 17:24:50 -0700

    adds service

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="landingPageHeader">
-        <h1>MBA Beauty Salon</h1>
+        <h1 className="companyName">MBA Beauty Salon</h1>
       </header>
-      <div className="services">
-        {/* <header className="servicesHeader">
-          <h1>Services</h1>
+      <div>
+        <header className="servicesHeader">
+          <h1 className="servicesTitle">Services</h1>
+          <hr />
         </header>
-        <div className="servicesList">
-          <h3>Threading</h3>
-          <h3>Hair and Color</h3>
-          <h3>Waxing</h3>
-          <h3>Facial</h3>
-          <h3>Heena</h3>
-          <h3>Eyelashes</h3>
-          <h3>Microblading</h3>
-          <h3>Nails</h3>
-        </div> */}
+        <div className="services">
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Threading</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Hair and Color</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Waxing</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Facial</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Heena</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Eyelashes</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Microblading</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="serviceCard">
+            <Card.Body>
+              <Card.Text>Nails</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
 
+      <hr />
       <div className="contact">
         <header>
           <h1>Visit Us</h1>
         </header>
         <div className="contactInfo">
-          <h3>1855 S. La Barbera Ave Mountain House, CA 95391</h3>
-          <h3>bindooveerendra@yahoo.com</h3>
+          <h3>{'1855 S. La Barbera Ave Mountain House, CA 95391'}</h3>
+          <h3>{'bindooveerendra@yahoo.com'}</h3>
           <div className="openingHours">
             <h3>OPENING HOURS</h3>
             <h3>
