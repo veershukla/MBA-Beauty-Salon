fba5d6153404b561b65564c197ed61f4236d2c32
Vikas Shukla <shuk.vk@gmail.com>
2020-09-18 22:52:52 -0700

    changes page to match business card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import salonLogo from './salonLogo.png'
 import './App.css';
 
 function App() {
+  const M = <span>M</span>;
   return (
     <div className="App">
       <header className="landingPageHeader">
-        <h1 className="companyName">MBA Beauty Salon</h1>
+        <div className="imageContainer">
+          <img src={salonLogo} className="image" alt="MBA Beauty Salon Logo"/>
+        </div>
+        <div >
+          <h1 className="companyName">MBA</h1>
+          <h2 className="companyName2">Beauty Salon</h2>
+          <h4 className="customers">{'*Men  *Women  *Kids  *Seniors'}</h4>
+        </div>
       </header>
       <div>
         <header className="servicesHeader">
@@ -65,8 +74,21 @@ function App() {
           <h1>Visit Us</h1>
         </header>
         <div className="contactInfo">
-          <h3>{'1360 Railroad Avenue, Livermore, CA'}</h3>
-          <h3>{'mbabeautysalon@gmail.com'}</h3>
+          <h3>{'1360 Railroad Avenue, Livermore, CA 94550'}</h3>
+          <h3>{'mbabeautysalon1@gmail.com'}</h3>
+          <h3>{'Landline: (925) 583 - 5226'}</h3>
+          <h3>
+            Cell:   
+          </h3>
+          <h3>
+            <span style={{ color: '#be342b', whiteSpace:"pre" }}>M</span>  (209) 627 - 6825{'   '}  
+          </h3>
+          <h3>
+            <span style={{ color: '#be342b', whiteSpace:"pre"  }}>B</span>  (408) 477 - 5011{'   '}
+          </h3>
+          <h3>
+            <span style={{ color: '#be342b', whiteSpace:"pre"  }}>A</span>  (916) 512 - 7439
+          </h3>
           <div className="openingHours">
             <h3>OPENING HOURS</h3>
             <h3>{'Mon - Sat: 11am - 7pm Sunday: Closed'}</h3>
